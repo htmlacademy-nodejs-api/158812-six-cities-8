dd17ec8fef5561a2aa896a19aa4b33a84c31ddbe
Alena Karenina <karena1q84@gmail.com>
2024-09-21 16:44:42 +0500

    feat: добавит потоки (stream). WIP

--- src/shared/libs/file-reader/tsv-file-reader.ts
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -1,10 +1,10 @@
 import {FileReader} from './file-reader.interface.js';
-import {readFileSync} from 'node:fs';
 import {Cities, Goods, Offer, HousesTypes, UserTypes} from '../../types/index.js';
 import EventEmitter from 'node:events';
+import {createReadStream} from 'node:fs';
 
 export class TSVFileReader extends EventEmitter implements FileReader {
-  private rawData = '';
+  private CHUNK_SIZE = 16384; // 16KB
 
   constructor(
     private readonly filename: string,
@@ -12,73 +12,80 @@ export class TSVFileReader extends EventEmitter implements FileReader {
     super();
   }
 
-  public read(): void {
-    try {
-      this.rawData = readFileSync(this.filename, 'utf-8');
-    } catch (error: unknown) {
-      console.error(`Can't read file from path ${this.filename}.`);
+  private parseLineToOffer(line: string): Offer {
+    const [
+      title,
+      description,
+      uploadDate,
+      city,
+      previewImage,
+      images,
+      isPremium,
+      isFavorite,
+      rating,
+      type,
+      bedrooms,
+      guests,
+      price,
+      goods,
+      authorName,
+      email,
+      avatarUrl,
+      password,
+      userType,
+      latitude,
+      longitude
+    ] = line.split('\t');
 
-      if (error instanceof Error) {
-        console.error(error.message);
-      }
-    }
-  }
-
-  public toArray(): Offer[] {
-    if (!this.rawData) {
-      // Код для работы с потоками
-    }
-
-    return this.rawData
-      .split('\n')
-      .filter((row) => row.trim().length > 0)
-      .map((line) => line.split('\t'))
-      .map(([
-        title,
-        description,
-        uploadDate,
-        city,
-        previewImage,
-        images,
-        isPremium,
-        isFavorite,
-        rating,
-        type,
-        bedrooms,
-        guests,
-        price,
-        goods,
-        authorName,
+    return {
+      title,
+      description,
+      uploadDate,
+      city: city as Cities,
+      previewImage,
+      images: images.split(';'),
+      isPremium: Boolean(isPremium),
+      isFavorite: Boolean(isFavorite),
+      rating: Number(rating),
+      type: type as HousesTypes,
+      bedrooms: Number(bedrooms),
+      guests: Number(guests),
+      price: Number(price),
+      goods: goods.split(';') as Goods[],
+      author: {
+        name: authorName,
         email,
         avatarUrl,
         password,
-        userType,
-        latitude,
-        longitude]) =>
-        ({
-          title,
-          description,
-          uploadDate,
-          city: city as Cities,
-          previewImage,
-          images: images.split(';'),
-          isPremium: Boolean(isPremium),
-          isFavorite: Boolean(isFavorite),
-          rating: Number(rating),
-          type: type as HousesTypes,
-          bedrooms: Number(bedrooms),
-          guests: Number(guests),
-          price: Number(price),
-          goods: goods.split(';') as Goods[],
-          author: {
-            name: authorName,
-            email,
-            avatarUrl,
-            password,
-            type: userType as UserTypes,
-          },
-          coordinates: {latitude: Number(latitude), longitude: Number(longitude)},
-        })
-      );
+        type: userType as UserTypes,
+      },
+      coordinates: {latitude: Number(latitude), longitude: Number(longitude)},
+    };
+  }
+
+  public async read(): Promise<void> {
+    const readStream = createReadStream(this.filename, {
+      highWaterMark: this.CHUNK_SIZE,
+      encoding: 'utf-8',
+    });
+
+    let remainingData = '';
+    let nextLinePosition = -1;
+    let importedRowCount = 0;
+
+    for await (const chunk of readStream) {
+      remainingData += chunk.toString();
+
+      while ((nextLinePosition = remainingData.indexOf('\n')) >= 0) {
+        const completeRow = remainingData.slice(0, nextLinePosition + 1);
+        remainingData = remainingData.slice(++nextLinePosition);
+        importedRowCount++;
+
+        const parsedOffer = this.parseLineToOffer(completeRow);
+        this.emit('line', parsedOffer);
+      }
+    }
+
+    this.emit('end', importedRowCount);
   }
 }
