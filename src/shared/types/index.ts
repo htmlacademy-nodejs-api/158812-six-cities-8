a3093eb11b118cba3cd3f8e3cab984eb3ad08ff8
Alena Karenina <karena1q84@gmail.com>
2024-09-21 14:47:49 +0500

    feat: добавит запрос для получения данных. WIP

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -4,3 +4,5 @@ export * from './cities.enum.js';
 export * from './houses-types.enum.js';
 export * from './user.type.js';
 export * from './user-types.enum.js';
+
+export { MockServerData } from './mock-server-data.type.js';
