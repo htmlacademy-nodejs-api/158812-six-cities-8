e4fa5e28daffd77255c1abd1975ae49780d637d1
Alena Karenina <karena1q84@gmail.com>
2024-09-21 12:44:43 +0500

    feat: добавит тип MockServerData

---
+++ src/shared/types/mock-server-data.type.ts
@@ -0,0 +1,15 @@
+export type MockServerData = {
+  titles: string[];
+  descriptions: string[];
+  previewImages: string[];
+  images: string[];
+  cities: string[];
+  housesTypes: string[];
+  goods: string[];
+  usernames: string[];
+  emails: string[];
+  avatars: string[];
+  passwords: string[];
+  userTypes: string[];
+  coordinates: string[];
+};
