e28b007f751b46e9d08d8c2b856ab0d5b154786b
Alena Karenina <karena1q84@gmail.com>
2024-09-29 16:51:34 +0500

    feat: расширит интерфейс UserDocument. WIP

--- src/shared/modules/user/user.model.ts
+++ src/shared/modules/user/user.model.ts
@@ -1,13 +1,17 @@
 import { Schema, Document, model } from 'mongoose';
-import { User } from '../../types/index.js';
+import { User, UserTypes } from '../../types/index.js';
 
-export interface UserDocument extends User, Document {}
+export interface UserDocument extends User, Document {
+  createdAt: Date,
+  updatedAt: Date,
+}
 
 const userSchema = new Schema({
   email: String,
   avatarUrl: String,
-  firstname: String,
-  lastname: String,
-});
+  name: String,
+  password: String,
+  type: UserTypes
+}, { timestamps: true });
 
 export const UserModel = model<UserDocument>('User', userSchema);
