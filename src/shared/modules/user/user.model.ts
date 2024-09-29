1d0043b1bc6f10ec76aa2245ae4704da1321a8e1
Alena Karenina <karena1q84@gmail.com>
2024-09-29 17:13:35 +0500

    feat: задействует встроенную валидацию. WIP

--- src/shared/modules/user/user.model.ts
+++ src/shared/modules/user/user.model.ts
@@ -1,5 +1,5 @@
 import { Schema, Document, model } from 'mongoose';
-import { User, UserTypes } from '../../types/index.js';
+import { User } from '../../types/index.js';
 
 export interface UserDocument extends User, Document {
   createdAt: Date,
@@ -10,11 +10,29 @@ const userSchema = new Schema({
   email: {
     type: String,
     unique: true,
+    match: [/^([\w-\\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'Email is incorrect'],
+    required: true,
   },
-  avatarUrl: String,
-  name: String,
-  password: String,
-  type: UserTypes
+  avatarUrl: {
+    type: String,
+    required: false,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: [1, 'Min length for name is 1'],
+    maxlength: [15, 'Max length for name is 15'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: [6, 'Min length for password path is 6'],
+    maxlength: [12, 'Max length for password is 12'],
+  },
+  type: {
+    type: Boolean,
+    required: true,
+  }
 }, { timestamps: true });
 
 export const UserModel = model<UserDocument>('User', userSchema);
