48541cf336e987178cd6cc15f4067d240d45527f
Alena Karenina <karena1q84@gmail.com>
2024-09-29 17:39:57 +0500

    feat: добавит определение модели и схемы. WIP

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,9 +1,21 @@
+import { getModelForClass, prop } from '@typegoose/typegoose';
 import { User, UserTypes } from '../../types/index.js';
 
 export class UserEntity implements User {
+  @prop({ required: true })
   public name: string;
+
+  @prop({ unique: true, required: true })
   public email: string;
+
+  @prop({ required: false, default: '' })
   public avatarUrl: string;
+
+  @prop({ required: true })
   public type: UserTypes;
+
+  @prop({ required: true })
   public password: string;
 }
+
+export const UserModel = getModelForClass(UserEntity);
