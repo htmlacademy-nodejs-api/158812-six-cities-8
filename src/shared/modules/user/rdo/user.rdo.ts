052e3e336a3b116e079bfc248b7c67f38eae2859
Alena Karenina <karena1q84@gmail.com>
2024-10-27 16:46:11 +0500

    feat: added contoller to offer

--- src/shared/modules/user/rdo/user.rdo.ts
+++ src/shared/modules/user/rdo/user.rdo.ts
@@ -3,7 +3,7 @@ import { UserType } from '../../../types/user-type.enum.js';
 
 export class UserRdo {
   @Expose()
-  public email: string ;
+  public email: string;
 
   @Expose()
   public avatarUrl: string;
